35a89538267cb5104c836dec9f1dff45acdd4a78
Ahmed1891994 <ahmedhakem189@yahoo.com>
2022-11-15 02:29:02 +0200

    Change pom

--- src/test/java/pages/TestBase.java
+++ src/test/java/pages/TestBase.java
@@ -21,7 +21,7 @@ public class TestBase {
     @BeforeTest
     public void setupClass() {
     	BrowserFactoryManager browserfactorymanager = new BrowserFactoryManager();
-    	setDriverManager(browserfactorymanager.get("Firefox"));
+    	setDriverManager(browserfactorymanager.get("FireFox"));
     	setDriver(getDriverManager().getDriver());
         driverActions = new DriverActions(getDriver());
         getDriver().manage().window().maximize();
